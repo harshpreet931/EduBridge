ee3aac717df6495b1820714921a64aa081f66759
harshpreet931 <harshpreet0402@gmail.com>
2024-10-08 06:26:16 +0530

    Course Gets Activated

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -14,8 +14,8 @@ export default function Course({course}) {
   const { isLoading } = useWeb3()  
   const { account } = useAccount()
   const { ownedCourse } = useOwnedCourse(course, account.data)
-  const courseState = ownedCourse.data?.state
-  // const courseState = "activated"
+  let courseState = ownedCourse.data?.state
+  courseState = "purchased" === courseState ? "activated" : courseState
 
   const isLocked =
     !courseState ||
@@ -40,7 +40,6 @@ export default function Course({course}) {
           { courseState === "purchased" &&
             <Message type="warning">
               Course is purchased and waiting for the activation. Process can take up to 24 hours.
-              <i className="block font-normal">In case of any questions, please contact info@eincode.com</i>
             </Message>
           }
           { courseState === "activated" &&
@@ -52,7 +51,6 @@ export default function Course({course}) {
             <Message type="danger">
               Course has been deactivated, due the incorrect purchase data.
               The functionality to watch the course has been temporaly disabled.
-              <i className="block font-normal">Please contact info@eincode.com</i>
             </Message>
           }
         </div>
